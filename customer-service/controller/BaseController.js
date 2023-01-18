523edd4c91f42718c72a03e74c93ab5899d19fa2
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-01-18 14:11:19 +0300

    Customer BaseController

--- customer-service/controller/BaseController.js
+++ customer-service/controller/BaseController.js
@@ -1,24 +1,27 @@
 const { create, destroy, update, show, list } = require("../service/CustomerService")
 
 
+class BaseController {
+    
+    createCustomer = async (req, res) => {
 
+    }
 
-const createCustomer = async (req, res) => {
+    deleteCustomer = async (req, res) => {
 
+    }
+
+    updateCustomer = async (req, res) => {
+
+    }
+
+    showCustomer = async (req, res) => {
+
+    }
+
+    listCustomer = async (req, res) => {
+
+    }
 }
 
-const deleteCustomer = async (req, res) => {
 
-}
-
-const updateCustomer = async (req, res) => {
-
-}
-
-const showCustomer = async (req, res) => {
-
-}
-
-const listCustomer = async (req, res) => {
-
-}
